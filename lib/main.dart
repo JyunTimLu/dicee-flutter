ce2147bdb4bdfef645734759309fa3729bf7fceb
TimLu <lutim102030@gmail.com>
2020-03-08 23:07:28 +0800

    done dice practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,58 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
+          backgroundColor: Colors.white12,
+          appBar: AppBar(
+            title: Text('Dicee'),
+            backgroundColor: Colors.white12,
+          ),
+          body: DicePage()),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+
+  var rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset("images/dice$leftDiceNumber.png"),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset("images/dice$rightDiceNumber.png"),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
